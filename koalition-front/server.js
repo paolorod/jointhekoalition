12ecca8d7165b937b0bc78a54213cdae0a1da69c
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-07-29 23:56:47 +0200

    setting up handlebars

--- koalition-front/server.js
+++ koalition-front/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const exphbs  = require('express-handlebars');
+const hbs = exphbs.create({ /* config */ });
 
 // Constants
 const PORT = 3000;
@@ -9,13 +11,17 @@ const VIEWS = __dirname + '/views'
 
 // App
 const app = express();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/images', express.static(__dirname + '/images'));
 
 
-app.get('/', (req, res) => {
-  res.sendFile(VIEWS+'/index.html');
+app.get('/', function (req, res) {
+  res.render('home');
 });
 
 app.listen(PORT, HOST);
