60663659ff940359fea1cdf1ed7d81effd27df05
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-07-29 19:01:15 +0200

    Refactoring and docker init

---
+++ koalition-front/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+const VIEWS = __dirname + '/views'
+
+// App
+const app = express();
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', (req, res) => {
+  res.sendFile(VIEWS+'/index.html');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
