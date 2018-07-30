6c6a6ad3d7b372223eb2c8e410721ad8ac88a6d8
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-07-30 12:11:55 +0200

    translation setup and navbar

--- koalition-front/server.js
+++ koalition-front/server.js
@@ -1,28 +1,44 @@
 'use strict';
 
-const express = require('express');
-const exphbs  = require('express-handlebars');
-const hbs = exphbs.create( {defaultLayout: 'main'} );
-
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
-const VIEWS = __dirname + '/views'
 
-// App
+// Express and Handlebars setup
+const express = require('express');
+const exphbs  = require('express-handlebars');
+const hbs = exphbs.create({ defaultLayout: "main"});
+
+
+// App creation and configuration
 const app = express();
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// static file serving
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/images', express.static(__dirname + '/images'));
 
+// A very basic translation support
+var translation = {
+  "ita" : require("./translations/italian.json"),
+  "fra" : require("./translations/french.json"),
+  "eng" : require("./translations/english.json"),
+  default : require("./translations/italian.json")
+}
 
+
+// Methods
 app.get('/', function (req, res) {
-  res.render('home');
+  res.redirect("/ita")
 });
 
+app.get('/:lang/', function (req, res) {
+  res.render('home',translation[req.params.lang]);
+});
+
+
+// Start
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
