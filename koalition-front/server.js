289513b6a0b96e56b0dd76659ac28640b9451736
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-08-28 07:54:01 +0200

    named form

--- koalition-front/server.js
+++ koalition-front/server.js
@@ -59,8 +59,8 @@ app.use('/images', express.static(__dirname + '/images'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 
 // body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var api_keys = require("./api_keys.json");
 
