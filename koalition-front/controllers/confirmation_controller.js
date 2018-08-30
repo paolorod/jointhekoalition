ca72b9d941c275bc93d187dd27847220b1c069d4
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-08-30 17:29:35 +0200

    child creation with callbacks done, missing proper error management

--- koalition-front/controllers/confirmation_controller.js
+++ koalition-front/controllers/confirmation_controller.js
@@ -9,43 +9,63 @@ function  execute_get(req,res) {
 function  execute_post(req,res) {
             
             var base = res.app.get("base")
-            
-            var body = req.body;
 
-            others_record_ids = [];
-            // add the record ids for linking
-            body.others.forEach(element => {
-                if(!(element.tag === "")) { 
-                    base('Other People Inside Confirmation').create({
-                        "Name and Surname": element.tag
-                    }, function(err, record) {
-                        others_record_ids.push(record.getId());
-                    });
-                } 
-            });
-            
-            // create main record
-            base('Automatic Confirmations').create({
+            var body = req.body;
+            console.log(req.body)
+
+            // create the main record object
+            var main_record = {
                 "Name": body.first_name,
                 "Surname": body.last_name,
                 "Phone Number": body.telephone,
                 "Email": body.email,
                 "Need Hostel": (body.need_hostel=="on"),
                 "Dinner of the 14th": (body.dinner_14=="on"),
-                "Other People Inside Confirmation": others_record_ids,
+                "Other People Inside Confirmation": [],
                 "Special Food Needs": body.particular_food,
                 "Special Mobility Needs": body.particular_mobility,
                 "Special Baby Needs": body.paritcular_babies,
                 "Message": body.message
-              }, function(err, record) {
-                  if (err) { console.error(err); return; }
-                  console.log(record.getId());
-              });
-
-              
-            console.log(req.body)
+              };
+        
+            add_childs(body.others,main_record,base)
+         
             res.send(req.body);    
 }
+
+function add_childs(childs,parent_record,base) {
+
+    if(childs.length > 0) {
+        current_child = childs.pop().tag;
+        if(!(current_child === "")) {
+            // is a good record, so create
+            console.log("Creating child - "+ current_child)
+            base('Other People Inside Confirmation').create({
+                "Name and Surname": current_child
+            }, function(err, child_record) {
+                if (err) { 
+                    console.error(err); return; 
+                }
+                // add to the recursion parent record reference the child id and call recursion through callback
+                parent_record["Other People Inside Confirmation"].push(child_record.getId());
+                add_childs(childs,parent_record,base);
+            });
+        } else {
+            // immediate recursion in case of invalid element
+            console.log("Invalid child - "+ current_child);
+            add_childs(childs,parent_record,base);
+        }
+    } else {
+        // all childs inserted, so create the parent record
+        console.log("Creating main record with childs "+ parent_record["Other People Inside Confirmation"])
+        base('Automatic Confirmations').create(parent_record, function(err, record) {
+              if (err) { 
+                  console.error(err); return; 
+              }
+          });
+    }
+}
+
 
 exports.execute_get = execute_get
 exports.execute_post = execute_post
