408af979e5e4cc886333084fb4c2e778789f7ffa
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-09-12 21:29:07 +0200

    mailer working but disabled via flag

--- koalition-front/controllers/confirmation_controller.js
+++ koalition-front/controllers/confirmation_controller.js
@@ -7,8 +7,9 @@ function  execute_get(req,res) {
         
 function  execute_post(req,res) {
             
-            var base = res.app.get("base")
             var body = req.body;
+
+            console.log(body)
 
             // input parsing
             var main_record = {
@@ -16,8 +17,8 @@ function  execute_post(req,res) {
                 "Surname": body.last_name,
                 "Phone Number": body.telephone,
                 "Email": body.email,
-                "Need Hostel": (body.need_hostel=="on"),
-                "Dinner of the 14th": (body.dinner_14=="on"),
+                "Need Hostel": (body.need_hostel==="on"),
+                "Dinner of the 14th": (body.dinner_14==="on"),
                 "Other People Inside Confirmation": [],
                 "Special Food Needs": body.particular_food,
                 "Special Mobility Needs": body.particular_mobility,
@@ -31,17 +32,19 @@ function  execute_post(req,res) {
                     other_participants.push(element.tag);
                   }
               });
-        
+
             // send to creation backend using a recursive call via callbacks
-            return create_records(main_record,other_participants,base,res);
+            return create_records(main_record,other_participants,0,res);
              
 }
 
 // recursive callback function to create the child records then the main record referring to them
-function create_records(main_record,other_participants,base,res) {
+function create_records(main_record,other_participants,participants_created,res) {
 
-    if(other_participants.length > 0) {
-        current_child = other_participants.pop();
+    var base = res.app.get("base")
+
+    if(participants_created < other_participants.length) {
+        current_child = other_participants[participants_created++];
         console.log("Creating child - "+ current_child)
         base('Other People Inside Confirmation').create({
             "Name and Surname": current_child
@@ -52,7 +55,7 @@ function create_records(main_record,other_participants,base,res) {
             }
             // add to the recursion parent record reference the child id and call recursion through callback
             main_record["Other People Inside Confirmation"].push(child_record.getId());
-            return create_records(main_record,other_participants,base,res);
+            return create_records(main_record,other_participants,participants_created,res);
         });
     } else {
         // all childs inserted, so create the parent record
@@ -62,7 +65,7 @@ function create_records(main_record,other_participants,base,res) {
                   console.error(err);
                   return send_error(res);  
               }
-            return send_confirmation_email(main_record,res);
+            return send_confirmation_email(main_record,other_participants,res);
           });
     }
 }
@@ -77,25 +80,40 @@ function send_error(res) {
     res.send("Error while writing records");
 }
 
-function send_confirmation_email(record,res) {
+function send_confirmation_email(record,other_participants,res) {
+    mail_flag = false
+    if(mail_flag) {
+        var transport = res.app.get("mailer-transport");
 
-    var transport = res.app.get("mailer-transport");
+        // restore the old array content in order to have a nice display in the email
+        record["Other People Inside Confirmation"] = other_participants
 
-    var mailOptions = {
-        from: 'confirmation@jointhekoalition.eu',
-        to: 'ariane.paolo@jointhekoalition.eu',
-        subject: '[Confirmation]',
-        text: 'That was easy!'
-      };
-      
-      transport.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-          return send_error(res);
-        } 
-        return send_success(res);
-      }); 
-      
+        var mailOptions = {
+            from: 'confirmation@jointhekoalition.eu',
+            to: 'ariane.paolo@jointhekoalition.eu',
+            subject: '[Confirmation] '+record["Name"] + " " + record["Surname"],
+            html: json_to_html(record)
+        };
+        
+        transport.sendMail(mailOptions, function(error, info){
+            if (error) {
+            console.log(error);
+            return send_error(res);
+            } 
+            return send_success(res);
+        });
+    } else {
+        send_success(res);
+    }
+}
+
+function json_to_html(object) {
+    html = "<table>"
+    for(x in object) {
+        html += "<tr><td><b>"+x+"</b></td><td>"+object[x]+"</td></tr>"
+    }
+    html += "</table>"
+    return html
 }
 
 
