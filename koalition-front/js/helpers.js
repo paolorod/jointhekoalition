2a39340246547d401e7c0ce7b415aa5998085949
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-09-12 21:49:26 +0200

    corrected form value reading logic

--- koalition-front/js/helpers.js
+++ koalition-front/js/helpers.js
@@ -6,40 +6,52 @@ function prevent_enter_keypress(event) {
         if (e.key === 'Enter') e.preventDefault();
 }
 
+
 /** Retrieve the chips values, add them to the form and then execute post  */
 function confirmation_form_submit() {
 
+    fields = {
+          "first_name" : {"type":"text"},
+          "last_name" : {"type":"text"},
+          "telephone" :{"type":"telephone"},
+          "email": {"type":"email"},
+          "need_hostel": {"type":"checkbox","optional":true},
+          "dinner_14": {"type":"checkbox","optional":true},
+          "particular_food" : {"type":"text"},
+          "particular_mobility" : {"type":"text"},
+          "particular_babies" : {"type":"text"},
+          "message" : {"type":"text"}
+        };
 
-    ids = ["first_name",
-          "last_name",
-          "telephone",
-          "email",
-          "need_hostel",
-          "dinner_14",
-          "particular_food",
-          "particular_mobility",
-          "particular_babies",
-          "message"];
-    
-    data = {};
+    data = {}
 
-    ids.forEach(id => {
-        data[id] = $("#"+id).val();
-    });
+    for( id in fields) {
+        if (fields[id].type==="checkbox") {
+            data[id] = $("#"+id).prop("checked");
+        } else {
+            data[id] = $("#"+id).val();
+        }
+    }
 
-    // take chips data and extra eventual leftovers
-    chips_data = M.Chips.getInstance($('.chips')).chipsData;
-    chips_data.push({tag: $("#others").val()});
+    // take chips data and extra eventual leftovers with a deep clone
+    chips_data = JSON.parse(JSON.stringify(M.Chips.getInstance($('.chips')).chipsData));
+    chips_leftover = $("#others").val()
+    if(!chips_leftover.trim()==="") {
+        chips_data.push({tag: chips_leftover});
+    }
 
+    // save into main object
     data["others"] = chips_data;
+
+    alert(JSON.stringify(data))
 
      // construct an HTTP request
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "./confirmation", true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    //xhr.open("POST", "./confirmation", true);
+    //xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
     // send the collected data as JSON
-    xhr.send(JSON.stringify(data));
+    //xhr.send(JSON.stringify(data));
     
 
     xhr.onloadend = function () {
