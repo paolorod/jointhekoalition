5d68d5cffc68fd16e32c1a7c386c78d32004e1ef
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-11-25 23:02:51 +0100

    hostel addon

--- koalition-front/js/init.js
+++ koalition-front/js/init.js
@@ -4,5 +4,13 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    $('.carousel').carousel(
+      {numVisible: 3,
+       dist: -50
+      });
+    var autoplay = true;
+    setInterval(function() { if(autoplay) $('.carousel').carousel('next'); }, 4500);
+    $('.carousel').hover(function(){ autoplay = false; },function(){ autoplay = true; });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
