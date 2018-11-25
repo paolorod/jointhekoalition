a420a2c16b7129233bc275f8ccb4856378fed8c7
Paolo Rodeghiero <paolo.rod@gmail.com>
2018-11-25 16:31:51 +0100

    support for [upper] safe tag in format

--- koalition-front/helpers.js
+++ koalition-front/helpers.js
@@ -7,7 +7,8 @@ function format(text) {
     text = handlebars.escapeExpression(text)
     return new handlebars.SafeString(
       text.replace(/\[b\](.*)\[\/b\]/g, '<b>$1</b>')
-    )
+          .replace(/\[upper\](.*)\[\/upper\]/g, '<small><sup>$1</small>')
+     )
   }
 
 exports.format = format;
